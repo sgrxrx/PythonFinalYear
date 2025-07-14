fd8aca0b667424eb3736a3fc7a8d636d5593621e
sagar <sagarpokharel25@gmail.com>
2025-07-14 07:45:31 +0545

    issue resolved

--- apps/issues/views.py
+++ apps/issues/views.py
@@ -18,6 +18,16 @@ class IssueViewSet(viewsets.ModelViewSet):
     queryset = Issue.objects.all()
     serializer_class = IssueSerializer
 
+    # def get_queryset(self):
+    #     queryset = Issue.objects.all()
+    #     status = self.request.query_params.get('status')
+    #     user_specific = self.request.query_params.get('user_specific')
+
+    #     if status:
+    #         queryset = queryset.filter(status=status)
+    #     if user_specific == 'true':
+    #         queryset = queryset.filter(reported_by=self.request.user)
+    #     return queryset
     def get_queryset(self):
         queryset = Issue.objects.all()
         status = self.request.query_params.get('status')
@@ -25,8 +35,17 @@ class IssueViewSet(viewsets.ModelViewSet):
 
         if status:
             queryset = queryset.filter(status=status)
+
         if user_specific == 'true':
-            queryset = queryset.filter(reported_by=self.request.user)
+            # Check if current user is an authority
+            authority = Authority.objects.filter(user=self.request.user).first()
+            if authority and authority.issue_type:
+                # User is an authority, return issues of their type
+                queryset = queryset.filter(issue_type=authority.issue_type)
+            else:
+                # User is not an authority, return issues reported by them
+                queryset = queryset.filter(reported_by=self.request.user)
+
         return queryset
 
 
